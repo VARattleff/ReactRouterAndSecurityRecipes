65f3d6ce000b806d78c32a1f9ba1ccb61de94180
Viktor August Rattleff <varattleff@Viktors-MacBook-Air.local>
2024-03-07 15:17:31 +0100

    update add

--- src/recipes/AddCategory.tsx
+++ src/recipes/AddCategory.tsx
@@ -1,21 +1,22 @@
 import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {addCategory} from "../services/apiFacade.ts";
+import { addCategory } from "../services/apiFacade.ts";
 
 export function AddCategory() {
-    const categoryRef = useRef("");
+    const categoryRef = useRef<HTMLInputElement | null>(null);
     const navigate = useNavigate();
 
     async function submitNewCategory() {
-        const name = categoryRef.current?.value;
-        try {
-            await addCategory(name);
-            alert("Category added successfully");
-            navigate("/categories");
-            //@ts-ignore
-            categoryRef.current.value = "";
-        } catch (error) {
-            console.log("Present the error to the user: ", error);
+        if (categoryRef.current) {
+            const name = categoryRef.current.value;
+            try {
+                await addCategory(name);
+                alert("Category added successfully");
+                navigate("/categories");
+                categoryRef.current.value = "";
+            } catch (error) {
+                console.log("Present the error to the user: ", error);
+            }
         }
     }
 
@@ -23,7 +24,7 @@ export function AddCategory() {
         <>
             <h2>Add Category</h2>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" ref={categoryRef}/>
+            <input type="text" id="name" ref={categoryRef} />
             <button className={""} onClick={submitNewCategory}>
                 Add
             </button>
